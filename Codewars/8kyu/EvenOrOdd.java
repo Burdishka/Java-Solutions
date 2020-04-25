520eaae2e0546cb3cd38df3f266f153069edb2fc
Burdishka <oleksandr.burdeinyi@gmail.com>
2020-04-25 16:04:54 -0400

    8 kyu - Even or Odd

---
+++ Codewars/8kyu/EvenOrOdd.java
@@ -0,0 +1,15 @@
+/*
+8 kyu
+Even or Odd
+https://www.codewars.com/kata/53da3dbb4a5168369a0000fe
+
+Create a function (or write a script in Shell) that takes an integer as an argument
+and returns "Even" for even numbers or "Odd" for odd numbers.
+*/
+
+public class EvenOrOdd {
+    public static String even_or_odd(int number) {
+        if (number % 2 == 0) return "Even";
+        else return "Odd";   
+    }
+}
