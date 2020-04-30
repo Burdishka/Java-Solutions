7721e4d4bceabab0e5819b1a1b210b7f78bd74b0
Burdishka <oleksandr.burdeinyi@gmail.com>
2020-04-30 17:36:12 -0400

    8 kyu - Kata Example Twist

---
+++ Codewars/8kyu/KataExampleTwist.java
@@ -0,0 +1,17 @@
+/*
+8 kyu
+Kata Example Twist
+
+https://www.codewars.com/kata/525c1a07bb6dda6944000031/solutions/java
+*/
+
+public class KataExampleTwist
+{
+    public static String[] kataExampleTwist()
+    {
+        String[] websites = new String[1000];
+        for (int i = 0; i < websites.length; i++)
+          websites[i] = "codewars";
+        return websites;
+    }
+}
