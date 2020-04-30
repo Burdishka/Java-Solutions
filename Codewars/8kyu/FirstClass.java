b453dc8c5a65cd103b5d8982ed210003d08968df
Burdishka <oleksandr.burdeinyi@gmail.com>
2020-04-30 16:55:41 -0400

    8 kyu - A + B

---
+++ Codewars/8kyu/FirstClass.java
@@ -0,0 +1,13 @@
+/*
+8 kyu
+A + B
+
+https://www.codewars.com/kata/5512a0b0509063e57d0003f5
+*/
+
+public class FirstClass {
+    public static byte sum (byte a, byte b) {
+        byte c = (byte) (a + b);
+        return c;
+    }
+}
