251a6143c635b51f8c32dda81bf3b28436aeb678
Burdishka <oleksandr.burdeinyi@gmail.com>
2020-05-02 18:15:02 -0400

    8 kyu - Grasshopper - Basic Function Fixer

---
+++ Codewars/8kyu/Fixer.java
@@ -0,0 +1,12 @@
+/*
+8 kyu
+Grasshopper - Basic Function Fixer
+
+https://www.codewars.com/kata/56200d610758762fb0000002/java
+*/
+
+public class Fixer {
+  public static int addFive(int num) {
+    return num + 5;
+  }
+}
